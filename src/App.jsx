500309de8e08713f0f48fcea778fdd681cf81b12
Sunday P. Afolabi <spog4true@gmail.com>
2024-04-23 04:26:11 -0700

    Start react markup app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Main from "./layouts/Main"
+
+function App() {
+
+  return (
+    <Main />
+  )
+}
+
+export default App
---
+++ src/layouts/Main.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar";
+
+const Main = () => {
+    return (
+        // This is like a skeleton
+        // Navbar
+        // Content
+        // Footer
+        <>
+            <Navbar />
+            <h1>Main</h1>
+        </>
+    )
+}
+
+export default Main;
